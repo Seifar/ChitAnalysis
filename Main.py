f98e5a3a39cf384ec189242ff67d5db49d95f4cc
Thomas Frank <ga94ruq@mytum.de>
2018-07-21 11:59:15 +0200

    Initial commit

---
+++ Main.py
@@ -0,0 +1,85 @@
+import cv2
+import glob, os
+import numpy as np
+
+import imutils
+
+
+def showIMG(mat, name, delay=0):
+    cv2.namedWindow(name, cv2.WINDOW_KEEPRATIO)
+    cv2.imshow(name, mat)
+    cv2.resizeWindow(name, 1000, 1000)
+    cv2.waitKey(delay)
+    cv2.destroyWindow(name)
+
+def splitChit(original):
+    # edge detection
+    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(original, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+    #showIMG(edged, "Canny")
+    edged = cv2.GaussianBlur(edged, (3, 3), 0)
+
+    # finding contours
+    cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    print(len(cnts))
+    cnts = filter(lambda x: original.shape[0] * original.shape[1] / 6 < cv2.contourArea(x), cnts)
+    screenCnt = []
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            screenCnt.append(approx)
+    # show contours
+    #contourImage = original.copy()
+    #cv2.drawContours(contourImage, screenCnt, -1, (0, 255, 0), 5)
+    #showIMG(contourImage, "contours")
+
+    # create individual Images
+    chits = []
+    for c in screenCnt:
+        pts1 = [c[0][0], c[1][0], c[2][0], c[3][0]]
+        # sort the contour points in the right way
+        pts1 = sorted(pts1, key=lambda x: x[1])
+        if pts1[0][0] > pts1[1][0]:
+            pts1[0], pts1[1] = pts1[1], pts1[0]
+        if pts1[2][0] > pts1[3][0]:
+            pts1[2], pts1[3] = pts1[3], pts1[2]
+        # transform
+        pts1 = np.float32(pts1)
+        pts2 = np.float32([[0, 0], [2000, 0], [0, 1000], [2000, 1000]])
+        M = cv2.getPerspectiveTransform(pts1, pts2)
+        dst = cv2.warpPerspective(original, M, (2000, 1000))
+        chits.append(dst)
+    return chits
+
+#iput: single chit in landscape orientation (top either on the left or right)
+#returns: chit in portrait mode
+def rotateChit(chit):
+    rows, cols, _ = chit.shape
+    grey = cv2.cvtColor(chit, cv2.COLOR_BGR2GRAY)
+    leftfill = 0
+    for c in range(cols):
+        for r in range(round(rows*0.05)):
+            leftfill += grey[r][c]
+
+    rightfill = 0
+    for c in range(cols):
+        for r in range(round(rows*0.95), rows):
+            leftfill += grey[r][c]
+
+    return imutils.rotate_bound(chit, 90 if leftfill<rightfill else 270)
+
+os.chdir("data")
+for file in glob.glob("*.[Jj][Pp][Gg]"):
+    #display original file
+    original = cv2.imread(""+file)
+    showIMG(original, "original")
+
+    chits = splitChit(original)
+
+    chits = map(lambda x: rotateChit(x),chits)
+    #show them
+    for chit in chits:
+        showIMG(chit, "chit")
